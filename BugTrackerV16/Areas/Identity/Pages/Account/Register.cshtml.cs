f9b40d497f612faf605a9931470bd56de4420111
Iblal <81813762+Iblal@users.noreply.github.com>
2021-08-22 23:50:37 +0100

    Added role assignment to user registration

--- BugTrackerV16/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BugTrackerV16/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -52,6 +52,19 @@ namespace BugTrackerV16.Areas.Identity.Pages.Account
             public string Email { get; set; }
 
             [Required]
+            [Display(Name = "First Name")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; }
+
+            [Required]
+            [Display(Name = "Role")]
+            public string Role { get; set; }
+
+
+            [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
@@ -75,10 +88,12 @@ namespace BugTrackerV16.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new BugTrackerV16User { UserName = Input.Email, Email = Input.Email };
+                var user = new BugTrackerV16User { UserName = Input.Email, Email = Input.Email, FirstName = Input.FirstName, LastName = Input.LastName };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
+                    await _userManager.AddToRoleAsync(user, Input.Role);
+
                     _logger.LogInformation("User created a new account with password.");
 
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
